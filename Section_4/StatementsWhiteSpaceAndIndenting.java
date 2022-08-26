37a68a14f6e7e810eeb3e83328405010265c5b04
John DH. Park <john.dh.park@me.com>
2022-08-25 18:52:35 -0700

    Section 05

--- Section_4/StatementsWhiteSpaceAndIndenting.java
+++
@@ -1,9 +0,0 @@
-public class StatementsWhiteSpaceAndIndenting {
-	public static void main(String[] args) {
-		int myVariable = 50;
-		myVariable++;
-		myVariable--;
-		System.out.println("This is a test");
-
-	}
-}

e4ebcda5fc22894f8ad25739d488c68ec68925f8
John DH. Park <john.dh.park@icloud.com>
2023-08-17 03:14:30 -0700

    Additional Completed Lessons

--- Section_06/Switch/src/Main.java
+++ Section_06/Switch/src/Main.java
@@ -1,13 +1,35 @@
 class Main {
     public static void main(String[] args) {
-        int value = 1;
+        // int value = 1;
 
-        if (value == 1) {
-            System.out.println("Value was 1");
-        } else if (value == 2) {
-            System.out.println("Value was 2");
-        } else {
-            System.out.println("Was not 1 or 2");
+        // if (value == 1) {
+        // System.out.println("Value was 1");
+        // } else if (value == 2) {
+        // System.out.println("Value was 2");
+        // } else {
+        // System.out.println("Was not 1 or 2");
+        // }
+
+        int switchValue = 3;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Actually it was a " + switchValue);
+                // break;
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
         }
+
+        // More code here
     }
+
 }

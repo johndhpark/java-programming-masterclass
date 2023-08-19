23af226b57f1109217860d3b60e087932a9b3bb7
John DH. Park <john.dh.park@icloud.com>
2023-08-19 14:18:36 -0700

    Section 7: Videos 93 to 99 (Complete)

---
+++ Section_07/AllAboutStrings/src/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static void main(String[] args) {
+        printInformation("Hello World");
+        printInformation("");
+        printInformation("\t   \n");
+
+        String helloWorld = "Hello World";
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('r'));
+        System.out.printf("index of World = %d, %n", helloWorld.indexOf("World"));
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l'));
+        System.out.printf("last index of l = %d %n", helloWorld.lastIndexOf('l'));
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l'), 3);
+        System.out.printf("last index of l = %d %n", helloWorld.lastIndexOf('l'), 8);
+
+        String helloWordLower = helloWorld.toLowerCase();
+
+        if (helloWorld.equals(helloWordLower)) {
+            System.out.println("Values match exactly");
+        }
+        if (helloWorld.equalsIgnoreCase(helloWordLower)) {
+            System.out.println("Values match ignorning case");
+        }
+
+        if (helloWorld.startsWith("Hello")) {
+            System.out.println("String starts with Hello");
+        }
+
+        if (helloWorld.endsWith("World")) {
+            System.out.println("String ends with World");
+        }
+
+        if (helloWorld.contains("World")) {
+            System.out.println("String contains World");
+        }
+
+        if (helloWorld.contentEquals("Hello World")) {
+            System.out.println("Values match exactly");
+        } 
+
+    }
+
+    public static void printInformation(String string) {
+        int length = string.length();
+        System.out.printf("Length = %d %n", length);
+
+        if (string.isEmpty()) {
+            System.out.println("String is Empty");
+            return;
+        }
+
+        if (string.isBlank()) {
+            System.out.println("String is Blank");
+            return;
+        }
+
+        System.out.printf("First char = %c %n", string.charAt(0));
+
+        System.out.printf("Last char = %c %n", string.charAt(length - 1));
+
+    }
+}
